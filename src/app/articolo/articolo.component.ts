b86faa7901a0e901c1f85e02185e15022c827c99
ciomeim <ciomei.firebase@gmail.com>
2019-12-23 20:02:03 +0100

    1 file modified, 3 added

---
+++ src/app/articolo/articolo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Articolo }  from './articolo'
+@Component({
+  selector: 'app-articolo',
+  templateUrl: './articolo.component.html',
+  styleUrls: ['./articolo.component.css']
+})
+export class ArticoloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
