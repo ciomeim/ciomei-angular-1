4375a9878b0a1a08c59d9d49cb62346d977b85a7
ciomeim <ciomei.firebase@gmail.com>
2019-12-23 20:23:29 +0100

    3 files modified, 2 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,9 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ArticoloComponent } from './articolo/articolo.component';
-
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ArticoloComponent ],
+  declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/articolo/articolo.component.ts
+++ src/app/articolo/articolo.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { Articolo }  from './articolo'
+import { Articolo }  from '../articolo'
 @Component({
   selector: 'app-articolo',
   templateUrl: './articolo.component.html',
   styleUrls: ['./articolo.component.css']
 })
 export class ArticoloComponent implements OnInit {
-
-  constructor() { }
+  articolo: Articolo;
+  constructor() {
+    this.articolo = {
+            titolo: "Creare componenti Angular 2",
+            autore: "Mario Rossi",
+            testo:  "Creare componenti con Angular 2 è molto semplice."
+        };
+   }
 
   ngOnInit() {
   }
